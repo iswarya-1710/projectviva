b2f5117d5ab365bb0fbc15c46a9eb301db7e5a67
iswarya-1710 <iswaryasenthilkumar17@gmail.com>
2025-12-14 22:23:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def load_artifacts():
     num_cols = joblib.load("num_cols.joblib")
     cat_cols = joblib.load("cat_cols.joblib")
     return best_model, num_imp, feature_cols,num_cols,cat_cols
-best_model, num_imputer, feature_columns,num_cols,cat_cols = load_artifacts()
+best_model, num_imputer, feature_columns, num_cols, cat_cols = load_artifacts()
 
 st.sidebar.header("📌 Applicant Details")
 
